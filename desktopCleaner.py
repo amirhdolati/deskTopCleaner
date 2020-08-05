42b778747fa0d9b98f0b86d26b7b7c70198c0c31
AmirHossein <spark.one@hotmail.com>
2020-08-06 03:01:56 +0430

    fix bug and add 'Move' Function for readiblity and comfort

--- desktopCleaner.py
+++ desktopCleaner.py
@@ -9,7 +9,7 @@ import stat
 desktop_paths = [os.path.join(os.environ['USERPROFILE'], 'Desktop'),
                 os.path.join(os.environ['PUBLIC'], 'Desktop')] 
 
-dist_path = input("Give a path for backup:( for example : C:\\desktopBackup ) ")
+dist_path = input("Give a path for backup:( for example : X:\\yourDirectory ) ")
 
 
 ignore_list = ['desktopCleaner.py']
@@ -39,9 +39,15 @@ category = ['music', 'video', 'docoument', 'shortcut',
             'compressed', 'other']
 
 for folder in category:
-    new_path = os.path.join(dist_path,folder)
-    if not os.path.exists(path= new_path):
-        os.mkdir(path= new_path)   
+    try:
+        new_path = os.path.join(dist_path,folder)
+        if not os.path.exists(path= new_path):
+            os.mkdir(path= new_path) 
+    except FileNotFoundError:
+        print("this directory {} is not exist, please give a valid directory.".format(dist_path)) 
+        os.system('pause') 
+        exit()
+        
 
 
 time = datetime.date.today()
@@ -49,6 +55,13 @@ time = datetime.date.today()
 changed = 0
 moved_list = []
 
+def Move(firstPath, secondPath, name, File,  mode):
+    if mode == 0 :
+        shutil.move(firstPath,os.path.join(os.path.join(secondPath, name),
+        '{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,
+        chr(random.randint(97,122)),random.randint(1,100)) + File))
+    else:
+        shutil.move(firstPath ,os.path.join(secondPath, name))    
 
 for desktop_path in desktop_paths:
     files = os.listdir(path = desktop_path)
@@ -64,63 +77,63 @@ for desktop_path in desktop_paths:
                             os.chmod(full_path ,stat.S_IWRITE)
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'folder'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'folder'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'folder', File, 0)
                     else:
-                        shutil.move(source_path ,os.path.join(dist_path,'folder'))
+                        Move(source_path, dist_path, 'folder', File, 1)
                 elif re.search('^.*\.(txt|docx|doc|odm|dot|dotx|wri|pdf)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'docoument'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'docoument'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'docoument', File, 0)
                     else:
-                        shutil.move(source_path,os.path.join(dist_path,'docoument'))
+                        Move(source_path, dist_path, 'docoument', File, 1)
                 elif re.search('^.*\.(3gp|avi|mpeg|mpg|mpe|mp4|mkv|srt)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'video'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'video'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'video', File, 0)
                     else:
-                        shutil.move(source_path,os.path.join(dist_path,'video'))
+                        Move(source_path, dist_path, 'video', File, 1)
                 elif re.search('^.*\.(mp3|ogg|wma|wav)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'music'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'music'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'music', File, 0)
                     else:
-                        shutil.move(source_path,os.path.join(dist_path,'music'))
+                        Move(source_path, dist_path, 'music', File, 1)
                 elif re.search('^.*\.(url|lnk|sym)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'shortcut'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'shortcut'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
-                    else:    
-                        shutil.move(source_path ,os.path.join(dist_path,'shortcut'))
+                        Move(source_path, dist_path, 'shortcut', File, 0)
+                    else:   
+                        Move(source_path, dist_path, 'shortcut', File, 1) 
                 elif re.search('^.*\.(7z|apk|appx|bin|apk|appx|iso|rar|zip|xap|jar|lzip)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'compressed'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'compressed'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'compressed', File, 0)
                     else:    
-                        shutil.move(source_path ,os.path.join(dist_path,'compressed'))        
+                        Move(source_path, dist_path, 'compressed', File, 1)     
                 elif re.search('^.*\.(bmp|gif|ico|icon|png|jgp|psd|pdd|tiff|jpg|svg)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'picture'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'picture'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'picture', File, 0)
                     else:
-                        shutil.move(source_path ,os.path.join(dist_path,'picture'))
+                        Move(source_path, dist_path, 'picture', File, 1)
                 elif re.search('^.*\.(exe)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'executable'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'executable'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))   
+                        Move(source_path, dist_path, 'executable', File, 0) 
                     else: 
-                        shutil.move(source_path,os.path.join(dist_path,'executable'))
+                        Move(source_path, dist_path, 'executable', File, 1)
                 elif re.search('^.*\.(py|c|h|cpp|htm|html|css|js|php|java|vb|css)$',File):
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'code'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'code'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))        
+                        Move(source_path, dist_path, 'code', File, 0) 
                     else:      
-                        shutil.move(source_path ,os.path.join(dist_path,'code'))
+                        Move(source_path, dist_path, 'code', File, 1)
                 else:
                     changed+=1
                     if os.path.exists(os.path.join(os.path.join(dist_path,'other'),File)):
-                        shutil.move(source_path,os.path.join(os.path.join(dist_path,'other'),'{}-{}-{}_{}{}_'.format(time.year,time.month,time.day,chr(random.randint(97,122)),random.randint(1,100)) + File))
+                        Move(source_path, dist_path, 'other', File, 0)
                     else:
-                        shutil.move(source_path ,os.path.join(dist_path,'other'))
+                        Move(source_path, dist_path, 'other', File, 1)
                 moved_list.append(File)
             except PermissionError:
                 print("Access Denied ! give me Access to move this {} !".format(File))    
@@ -130,4 +143,5 @@ for desktop_path in desktop_paths:
 
 print("Done! {} file moved to desktopBackup folder!".format(changed))
 print("Moved file :\n{} ".format(moved_list))
-input("(Enter/return to exit)")
+print('bye bye :)')
+os.system('pause')
